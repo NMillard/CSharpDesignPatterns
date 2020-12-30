165bf7fc80f6d445fa4b629156c6ef1bd7bcb662
Nicklas Millard <n.millard@me.com>
2020-12-30 14:02:23 +0100

    add comment

--- src/Strategy/FileFormatting/ServiceInjector.cs
+++ src/Strategy/FileFormatting/ServiceInjector.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 
+/*
+ *  This is basically how you'd write the service injection for
+ *  registering the file formatters with the DI container.
+ */
+
 namespace FileFormatting {
     public static class ServiceInjector {
         public static IServiceCollection AddFileFormatters(this IServiceCollection services) {
